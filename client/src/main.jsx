578530bcb8e3616a004c39d70487e1a2611dce73
anthony <qiko0425@gmail.com>
2023-11-14 23:34:32 -0500

    Initial commit

---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Form, BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ThirdwebProvider desiredChainID={Sepolia}>
+    <Router>
+      <App />
+    </Router>
+  </ThirdwebProvider>
+);
